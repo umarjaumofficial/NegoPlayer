fd166f191cf2a7dcb58d30fb0f474807a82e650f
Umar <umarjaumofficial@gmail.com>
2026-03-10 00:39:39 +0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,18 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        // REQUIRED for yt-dlp and many advanced media libraries
+        maven { url = uri("https://jitpack.io") } 
     }
 }
 
